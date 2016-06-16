068cc953f1abf7a51573eb960a866739d2a3cfa5
wenxueliu <wenxueliu@wenxueliu.wenxueliu>
2016-06-16 11:55:29 -0500

    add ae

--- src/main.c
+++ src/main.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 
+#include "ae.h"
+
 void test() {
     printf("hello test\n");
 }
 
+void testPoll() {
+    printf("test epoll\n");
+    int size = 10;
+    aeEventLoop *ae = aeCreateEventLoop(size);
+}
+
 int main(int argc, char **argv) {
     test();
+    testPoll();
 }
